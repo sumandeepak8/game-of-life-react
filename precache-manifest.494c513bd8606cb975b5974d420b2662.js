b74794e731b027d8c2f3e6514c879d34c12144bf
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2019-04-05 13:57:40 +0530

    Updates

---
+++ precache-manifest.494c513bd8606cb975b5974d420b2662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3402fa1ba5cc5d61e3bc",
+    "url": "/game-of-life-react/static/js/runtime~main.a481a484.js"
+  },
+  {
+    "revision": "d78a4b36c3ab4da22552",
+    "url": "/game-of-life-react/static/js/main.3b09d3c7.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/game-of-life-react/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "d78a4b36c3ab4da22552",
+    "url": "/game-of-life-react/static/css/main.c2955483.chunk.css"
+  },
+  {
+    "revision": "68d7cf080b8c3a7c0ceffca442b35578",
+    "url": "/game-of-life-react/index.html"
+  }
+];
